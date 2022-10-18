c21956815f75e68149eacf41bc3fedab228da2f9
Olekasandr <74654117+Alex1120001@users.noreply.github.com>
2022-10-18 13:47:19 +0300

    Add files via upload

--- script/js.js
+++ script/js.js
@@ -1,3 +1,21 @@
+
+// якір
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+// перемикач
+
 let sw = document.querySelector('.sw');
 
 function func() {
@@ -18,3 +36,4 @@ function func() {
         premium.innerHTML = '1100грн';
     }
 }
+
